0d0d81c54d8c6cb7de9f8643f09f4134743a074a
b-zapata <150874080+b-zapata@users.noreply.github.com>
2024-03-08 21:46:09 -0700

    Finished cards and JSON connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as basketballTeamsData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface BasketballTeams {
+  teams: TeamProps[];
+}
+
+function Welcome() {
+  return <h1>These are the NCAA March Madness teams!</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Welcome />
+      <TeamsList />
+    </div>
+  );
+}
 
-        <p>Heyo</p>
-      </header>
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="team-card">
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamsList() {
+  const basketballTeams: BasketballTeams = basketballTeamsData;
+
+  return (
+    <div className="teams-list">
+      {basketballTeams.teams.map((oneTeam) => (
+        <Team key={oneTeam.tid} {...oneTeam} />
+      ))}
     </div>
   );
 }
